107ec7ed15f89fab78d51157a54e0e597843664f
Ramen Biswas <vercel@devsramen.com>
2025-06-04 00:16:49 +0600

    Update OfferComponent.jsx

--- src/components/OfferComponent.jsx
+++ src/components/OfferComponent.jsx
@@ -50,16 +50,51 @@ const OfferComponent = () => {
               >
                 Special Christmas Day Offer
               </p>
-              <div className="w-[306px] h-[267px] bg-[#D9D9D9] float-right"></div>
+              <div className="w-[306px] h-[267px] bg-[#D9D9D9] ms-auto"></div>
             </div>
-            <Link
-              to={"/"}
-              className="w-[505px] h-[490px] bg-[#F3F4F6] rounded-[21px]"
-            ></Link>
-            <Link
-              to={"/"}
-              className="w-[712px] h-[490px] bg-[#F3F4F6] rounded-[21px]"
-            ></Link>
+            <div
+              className={`w-[505px] h-[490px] bg-[#F3F4F6] rounded-[21px] pl-[59px] pr-[40px] pt-[58px] pb-[42px] relative`}
+            >
+              <div className={`w-[217px] absolute left-[59px] bottom-[80px]`}>
+                <Button
+                  buttonText="Discover Now"
+                  bg="bg-[#5E1EE5]"
+                  textColor="text-white"
+                  isArrowIcon={true}
+                />
+              </div>
+              <h3
+                className={`w-[309px] font-bold text-[39px] leading-[41.73px] tracking-[0.78px] pb-[21px]`}
+              >
+                Control Your Daily Activity On Yourself
+              </h3>
+              <div className="w-[263px] h-[243px] bg-[#D9D9D9] ms-auto"></div>
+            </div>
+            <div
+              className={`w-[712px] h-[490px] bg-[#F3F4F6] rounded-[21px] pl-[45px] pr-[36px] pt-[58px] pb-[31px] relative`}
+            >
+              <div className={`w-[217px] absolute right-[29px] bottom-[110px]`}>
+                <Button
+                  buttonText="Discover Now"
+                  bg="bg-[#5E1EE5]"
+                  textColor="text-white"
+                  isArrowIcon={true}
+                />
+              </div>
+              <h3
+                className={`w-[441px] font-bold text-[57px] leading-[66.69px] tracking-[1.14px] text-right mb-6 ms-auto`}
+              >
+                Travels Light, Sounds Heavy
+              </h3>
+              <div className="flex justify-between">
+                <div className="w-[371px] h-[243px] bg-[#D9D9D9] "></div>
+                <p
+                  className={`w-[220px] font-["montserrat"] font-semibold text-[23px] leading-[28.75px] tracking-[0.46px] mt-3 text-right`}
+                >
+                  20+ hours of portable playtime
+                </p>
+              </div>
+            </div>
 
             {/* {ServiceData.map((item, index) => (
               <Link
