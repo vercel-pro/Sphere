29b1031a190d3c5eb7feb264a49de3486e1ed30d
Ramen Biswas <vercel@devsramen.com>
2025-06-04 22:49:44 +0600

    Update BestSellers.jsx

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -4,8 +4,22 @@ import NewArrivalCartComponent from "./NewArrivalCartComponent";
 import IllustrationOne from "../assets/Illustration/IllustrationOne";
 import Stare from "../assets/Illustration/Stare";
 import { BestSellersData } from "../data/BestSellersData";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const BestSellers = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2500,
+    // autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+
   return (
     <>
       <div className="mt-24">
@@ -22,7 +36,33 @@ const BestSellers = () => {
             Flash sale for <span className="text-brightPurple">best</span>{" "}
             sellers
           </h2>
-          <div className={`flex items-center gap-[20px]`}>
+          <Slider {...settings}>
+            {BestSellersData.map((item, index) => (
+              <div className="">
+                <NewArrivalCartComponent
+                  key={index}
+                  cardWidth={"w-[390px]"}
+                  iconTop={item.iconTop}
+                  iconRight={item.iconRight}
+                  paddingLeft={item.paddingLeft}
+                  paddingRight={item.paddingRight}
+                  paddingTop={item.paddingTop}
+                  paddingBottom={item.paddingBottom}
+                  title={item.title}
+                  price={item.price}
+                  description={item.description}
+                  rating={item.rating}
+                  totalRating={item.totalRating}
+                  buttonText={item.buttonText}
+                  buttonBorder={item.buttonBorder}
+                  isDiscount={item.isDiscount}
+                  discountPercentage={item.discountPercentage}
+                  discountAmount={item.discountAmount}
+                />
+              </div>
+            ))}
+          </Slider>
+          {/* <div className={`flex items-center gap-[20px]`}>
             {BestSellersData.map((item, index) => (
               <NewArrivalCartComponent
                 key={index}
@@ -44,7 +84,7 @@ const BestSellers = () => {
                 discountAmount={item.discountAmount}
               />
             ))}
-          </div>
+          </div> */}
         </Container>
       </div>
     </>
