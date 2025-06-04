6f18501fa8c03483f12d59f4a3151444df49d2d5
Vercel <vercel@devsramen.com>
2025-06-04 16:11:52 +0600

    Update New Arrival Component & tailwind file at 04-06-2025_1611

--- src/components/NewArrivalComponent.jsx
+++ src/components/NewArrivalComponent.jsx
@@ -10,74 +10,47 @@ import NewArrivalCartComponent from "./NewArrivalCartComponent";
 import { NewArrivalData } from "../data/NewArrivalData";
 import { NewArrivalMenuData } from "../data/NewArrivalMenuData";
 
-// function SamplePrevArrow(props) {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div
-//       className={className}
-//       style={{
-//         ...style,
-//         color: "#303030",
-//         fontSize: "20px",
-//         border: "1px solid #303030",
-//         borderRadius: "50%",
-//         width: "40px",
-//         height: "40px",
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         position: "absolute",
-//         left: "calc(50% - 803px)",
-//         zIndex: "99",
-//       }}
-//       onClick={onClick}
-//     >
-//       {/* <MdArrowBackIosNew /> */}
-//     </div>
-//   );
-// }
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <Link
+      className={`w-[55px] h-[55px] flex justify-center items-center border border-black rounded-full transition-all duration-300 hover:border-transparent hover:bg-lightYellowGreen absolute right-[79px] -top-[103px] z-10`}
+      onClick={onClick}
+    >
+      <LeftArrow />
+    </Link>
+  );
+}
 
-// function SampleNextArrow(props) {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div
-//       className={className}
-//       style={{
-//         ...style,
-//         color: "#303030",
-//         fontSize: "20px",
-//         border: "1px solid #303030",
-//         borderRadius: "50%",
-//         width: "40px",
-//         height: "40px",
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         position: "absolute",
-//         left: "calc(50% + 745px)",
-//         zIndex: "99",
-//       }}
-//       onClick={onClick}
-//     >
-//       {/* <MdArrowForwardIos /> */}
-//     </div>
-//   );
-// }
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <Link
+      className={`w-[55px] h-[55px] flex justify-center items-center border border-black rounded-full transition-all duration-300 hover:border-transparent hover:bg-lightYellowGreen absolute right-[0] -top-[103px] z-10`}
+      onClick={onClick}
+    >
+      <RightShotArrowIcon />
+      {/* <MdArrowForwardIos /> */}
+    </Link>
+  );
+}
 
 const NewArrivalComponent = () => {
   const [menuData, setMenuData] = useState(NewArrivalMenuData);
 
-  // var settings = {
-  //   dots: false,
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 2,
-  //   slidesToScroll: 1,
-  //   autoplay: false,
-  //   autoplaySpeed: 2000,
-  //   nextArrow: <SampleNextArrow />,
-  //   prevArrow: <SamplePrevArrow />,
-  // };
+  var settings = {
+    // centerMode: true,
+    // centerPadding: "10px",
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
 
   const handleClick = (e) => {
     const indexNumber = parseInt(e.currentTarget.getAttribute("data-index"));
@@ -97,7 +70,7 @@ const NewArrivalComponent = () => {
             <h2 className="font-prompt font-bold text-[47px] leading-[58.75px] tracking-[-0.94px]">
               New <span className="text-brightPurple">arrival</span> for you
             </h2>
-            <div className="flex justify-end items-center gap-x-6">
+            {/* <div className="flex justify-end items-center gap-x-6">
               <Link
                 to={"/"}
                 className="w-[55px] h-[55px] flex justify-center items-center border border-black rounded-full transition-all duration-300 hover:border-transparent hover:bg-lightYellowGreen"
@@ -110,11 +83,11 @@ const NewArrivalComponent = () => {
               >
                 <RightShotArrowIcon />
               </Link>
-            </div>
+            </div> */}
           </div>
-          <div className="flex justify-between items-center mt-[46px]">
+          <div className="flex justify-between mt-[46px]">
             <div
-              className={`w-[321px] pl-6 relative before:content="" before:w-[2px] before:h-[308px] before:bg-[#E2E2E2] before:absolute before:left-0 before:top-1/2 before:-translate-1/2`}
+              className={`w-[321px] pt-8 pl-6 relative before:content="" before:w-[2px] before:h-[308px] before:bg-[#E2E2E2] before:absolute before:left-0 before:top-5`}
             >
               <ul
                 className={`flex flex-col gap-y-[30px] font-["montserrat"] font-medium text-base leading-[20.8px] text-paraColor `}
@@ -139,8 +112,8 @@ const NewArrivalComponent = () => {
                 ))}
               </ul>
             </div>
-            <div className="w-[919px]">
-              <div className="flex gap-[98px]">
+            <div className="w-[939px]">
+              {/* <div className="flex gap-[98px]">
                 {NewArrivalData.map((item, index) => (
                   <NewArrivalCartComponent
                     key={index}
@@ -159,24 +132,32 @@ const NewArrivalComponent = () => {
                     buttonBorder={item.buttonBorder}
                   />
                 ))}
+              </div> */}
+              <div className="slider-container ">
+                <Slider {...settings} className="justify-center">
+                  {NewArrivalData.map((item, index) => (
+                    <div className="py-4 px-10">
+                      <NewArrivalCartComponent
+                        cardWidth="w-[410px]"
+                        key={index}
+                        iconTop={item.iconTop}
+                        iconRight={item.iconRight}
+                        paddingLeft={item.paddingLeft}
+                        paddingRight={item.paddingRight}
+                        paddingTop={item.paddingTop}
+                        paddingBottom={item.paddingBottom}
+                        title={item.title}
+                        price={item.price}
+                        description={item.description}
+                        rating={item.rating}
+                        totalRating={item.totalRating}
+                        buttonText={item.buttonText}
+                        buttonBorder={item.buttonBorder}
+                      />
+                    </div>
+                  ))}
+                </Slider>
               </div>
-              {/* <Slider {...settings} className="flex justify-center">
-                {NewArrivalData.map((item, index) => (
-                  <NewArrivalCartComponent
-                    key={index}
-                    iconTop={item.iconTop}
-                    iconRight={item.iconRight}
-                    paddingLeft={item.paddingLeft}
-                    paddingRight={item.paddingRight}
-                    paddingTop={item.paddingTop}
-                    paddingBottom={item.paddingBottom}
-                    title={item.title}
-                    price={item.price}
-                    description={item.description}
-                    rating={item.rating}
-                  />
-                ))}
-              </Slider> */}
             </div>
           </div>
         </Container>
